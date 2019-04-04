0ae8104fe692dc7bddcdc12b5364f989883cf35a
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-04-04 18:36:19 +0530

    Fix NPE when textfield isnt supplied

--- lib/search_widget.dart
+++ lib/search_widget.dart
@@ -124,34 +124,36 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
 
   @override
   Widget build(BuildContext context) {
-    print(widget.queryBuilder);
     listContainerHeight = widget.listContainerHeight ?? MediaQuery.of(context).size.height / 4;
-    textField = widget.textFieldBuilder(_controller, _focusNode) ??
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: TextField(
-            controller: _controller,
-            focusNode: _focusNode,
-            style: new TextStyle(fontSize: 16, color: Colors.grey[600]),
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0x4437474F)),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Theme.of(context).primaryColor),
-              ),
-              suffixIcon: Icon(Icons.search),
-              border: InputBorder.none,
-              hintText: "Search here...",
-              contentPadding: EdgeInsets.only(
-                left: 16,
-                right: 20,
-                top: 14,
-                bottom: 14,
-              ),
-            ),
+    textField = widget.textFieldBuilder != null
+        ? widget.textFieldBuilder(_controller, _focusNode)
+        : Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: TextField(
+        controller: _controller,
+        focusNode: _focusNode,
+        style: new TextStyle(fontSize: 16, color: Colors.grey[600]),
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0x4437474F)),
           ),
-        );
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Theme
+                .of(context)
+                .primaryColor),
+          ),
+          suffixIcon: Icon(Icons.search),
+          border: InputBorder.none,
+          hintText: "Search here...",
+          contentPadding: EdgeInsets.only(
+            left: 16,
+            right: 20,
+            top: 14,
+            bottom: 14,
+          ),
+        ),
+      ),
+    );
 
     Column column = Column(
       mainAxisSize: MainAxisSize.min,
