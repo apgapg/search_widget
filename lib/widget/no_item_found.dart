75b950269fc1cb67caa20033ff7c678532cd8443
LINUX-XPS <jesusrope98@outlook.com>
2019-04-04 12:32:18 +0200

    Updated NoItemFound widget

--- lib/widget/no_item_found.dart
+++ lib/widget/no_item_found.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class NoItemFound extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const NoItemFound({
+    this.title = "No data found",
+    this.icon = Icons.folder_open,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Icon(
-            Icons.folder_open,
-            size: 24,
-            color: Colors.grey[900].withOpacity(0.7),
-          ),
+          Icon(icon, size: 24, color: Colors.grey[900].withOpacity(0.7)),
           SizedBox(width: 10.0),
           Text(
-            "No data found",
+            title,
             style: TextStyle(
               fontSize: 16.0,
               color: Colors.grey[900].withOpacity(0.7),
