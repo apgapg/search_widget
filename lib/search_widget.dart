eaac2c0686650ac7479adad4d3ae27e8ae386787
amir504036 <93222040+amir504036@users.noreply.github.com>
2022-01-27 02:43:04 +0330

    Update search_widget.dart

--- lib/search_widget.dart
+++ lib/search_widget.dart
@@ -1,9 +1,10 @@
 library search_widget;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
-import 'widget/no_item_found.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+// https://github.com/apgapg/search_widget/issues/15
 
 typedef QueryListItemBuilder<T> = Widget Function(T item);
 typedef OnItemSelected<T> = void Function(T item);
@@ -108,9 +109,7 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
         }
         _tempList.addAll(filterList);
         if (overlayEntry == null) {
-          if (_focusNode.hasFocus) {
-            onTap();
-          }
+          onTap();
         } else {
           overlayEntry.markNeedsBuild();
         }
@@ -335,3 +334,125 @@ class MySingleChoiceSearchState<T> extends State<SearchWidget<T>> {
     super.dispose();
   }
 }
+
+class NoItemFound extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const NoItemFound({
+    this.title = "No data found",
+    this.icon = Icons.folder_open,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon, size: 24, color: Colors.grey[900].withOpacity(0.7)),
+          SizedBox(width: 10.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.grey[900].withOpacity(0.7),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/// A base class to handle the subscribing events
+class KeyboardVisibilitySubscriber {
+  /// Called when a keyboard visibility event occurs
+  /// Is only called when the state changes
+  /// The [visible] parameter reflects the new visibility
+  final Function(bool visible) onChange;
+
+  /// Called when the keyboard appears
+  final Function onShow;
+
+  /// Called when the keyboard closes
+  final Function onHide;
+
+  /// Constructs a new [KeyboardVisibilitySubscriber]
+  KeyboardVisibilitySubscriber({this.onChange, this.onShow, this.onHide});
+}
+
+/// The notification class that handles all information
+class KeyboardVisibilityNotification {
+  static const EventChannel _keyboardVisibilityStream =
+      const EventChannel('flutter_keyboard_visibility');
+  static Map<int, KeyboardVisibilitySubscriber> _list =
+      Map<int, KeyboardVisibilitySubscriber>();
+  static StreamSubscription _keyboardVisibilitySubscription;
+  static int _currentIndex = 0;
+
+  /// The current state of the keyboard visibility. Can be used without subscribing
+  bool isKeyboardVisible = false;
+
+  /// Constructs a new [KeyboardVisibilityNotification]
+  KeyboardVisibilityNotification() {
+    _keyboardVisibilitySubscription ??= _keyboardVisibilityStream
+        .receiveBroadcastStream()
+        .listen(onKeyboardEvent);
+  }
+
+  /// Internal function to handle native code channel communication
+  void onKeyboardEvent(dynamic arg) {
+    isKeyboardVisible = (arg as int) == 1;
+
+    // send a message to all subscribers notifying them about the new state
+    _list.forEach((subscriber, s) {
+      try {
+        if (s.onChange != null) {
+          s.onChange(isKeyboardVisible);
+        }
+        if ((s.onShow != null) && isKeyboardVisible) {
+          s.onShow();
+        }
+        if ((s.onHide != null) && !isKeyboardVisible) {
+          s.onHide();
+        }
+      } catch (_) {}
+    });
+  }
+
+  /// Subscribe to a keyboard visibility event
+  /// [onChange] is called when a change of the visibility occurs
+  /// [onShow] is called when the keyboard appears
+  /// [onHide] is called when the keyboard disappears
+  /// Returns a subscribing id that can be used to unsubscribe
+  int addNewListener(
+      {Function(bool) onChange, Function onShow, Function onHide}) {
+    _list[_currentIndex] = KeyboardVisibilitySubscriber(
+        onChange: onChange, onShow: onShow, onHide: onHide);
+    return _currentIndex++;
+  }
+
+  /// Subscribe to a keyboard visibility event using a subscribing class [subscriber]
+  /// Returns a subscribing id that can be used to unsubscribe
+  int addNewSubscriber(KeyboardVisibilitySubscriber subscriber) {
+    _list[_currentIndex] = subscriber;
+    return _currentIndex++;
+  }
+
+  /// Unsubscribe from the keyboard visibility events
+  /// [subscribingId] has to contain an id previously returned on add
+  void removeListener(int subscribingId) {
+    _list.remove(subscribingId);
+  }
+
+  /// Internal function to clear class on dispose
+  dispose() {
+    if (_list.length == 0) {
+      _keyboardVisibilitySubscription?.cancel()?.catchError((e) {});
+      _keyboardVisibilitySubscription = null;
+    }
+  }
+}
+
